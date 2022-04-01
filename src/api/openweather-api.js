3d1fd4b1a6bb2a907f790ea5d365e5ad85515c17
Carlos Martinez <carlosde362@gmail.com>
2022-04-01 10:17:05 +0200

    added service to get weather

---
+++ src/api/openweather-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const TOKEN_API = 'e2cd17e960a8e30856d9602c73d23626';
+const OPTIONS_API = 'units=metric&lang=es';
+const EXCLUDE_API = 'exclude=minutely,hourly,current,alerts,flags';
+const API_URL_BASE = 'https://api.openweathermap.org/data/2.5';
+
+export async function getCurrentWeatherFromCity(city) {
+    const query=`${API_URL_BASE}/weather?q=${city}&appid=${TOKEN_API}&${OPTIONS_API}`;
+    const response = await axios.get(query);
+    const weatherNextDays = await getWeatherNextFourDays(response.data.coord.lon, response.data.coord.lat);
+    return {
+        current: response.data,
+        nextDays: weatherNextDays.slice(0, 4)
+    };
+}
+
+export async function getWeatherNextFourDays(lon, lat) {
+    const query = `${API_URL_BASE}/onecall?&lat=${lat}&lon=${lon}&appid=${TOKEN_API}&${OPTIONS_API}&${EXCLUDE_API}`;
+    const response = await axios.get(query);
+    return response.data.daily;
+}
