894c1fa13218347a47d5afda10acf9263fdf6697
Carlos Martinez <carlosde362@gmail.com>
2022-04-17 20:59:34 +0200

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SearchInput from './components/SearchInput'
 import { getCurrentWeatherFromCity } from './api/openweather-api';
 import CardCurrentWeather from './components/CardCurrentWeather';
 import CardNextDays from './components/CardNextDays';
+import Footer from './components/Footer';
 
 function App() {
   const [valueInput, setValueInput] = useState('');
@@ -19,11 +20,15 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col items-center pt-4 gap-4">
-      <SearchInput value={changeValue} />
-      <CardCurrentWeather weather={currentWeather} />
-      <CardNextDays weather={nextDaysWeather} />
+    <div className='flex flex-col justify-between h-full w-full bg-zinc-700 '>
+      <div className="flex flex-col items-center pt-4 gap-4">
+        <SearchInput value={changeValue} />
+        <CardCurrentWeather weather={currentWeather} />
+        <CardNextDays weather={nextDaysWeather} />
+      </div>
+      <Footer />
     </div>
+
   )
 }
 
